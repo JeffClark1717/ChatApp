c229f9731b8ab241303e06e682b077f155b37c94
Jeff Clark <116308210+JeffClark1717@users.noreply.github.com>
2023-11-22 14:23:24 -0800

    4.3 (not finished)

--- App.js
+++ App.js
@@ -1,29 +1,61 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Start from "./components/Start";
-import Chat from "./components/Chat";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Start from './components/Start'
+import Chat from './components/Chat';
+import { getFirestore } from "firebase/firestore";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from 'firebase/app';
+import { LogBox } from 'react-native';
 
 const Stack = createNativeStackNavigator();
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
+
+
 const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBtiJRPNM-6kw4WY7u7GWXeJQpuv89vqpc",
+    authDomain: "chatapp-8f1df.firebaseapp.com",
+    projectId: "chatapp-8f1df",
+    storageBucket: "chatapp-8f1df.appspot.com",
+    messagingSenderId: "816560297114",
+    appId: "1:816560297114:web:abacae15dcf0eed2e7bb23",
+    measurementId: "G-WTGXF4N801"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Start">
-        <Stack.Screen name="Start" component={Start}></Stack.Screen>
-        <Stack.Screen name="Chat" component={Chat}></Stack.Screen>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+      >
+        <Stack.Screen
+          name="Welcome"
+          component={Start}
+        />
+        <Stack.Screen
+          name="Chat">
+          {props => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+}
 
 export default App;
+
+
+/*
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtiJRPNM-6kw4WY7u7GWXeJQpuv89vqpc",
+  authDomain: "chatapp-8f1df.firebaseapp.com",
+  projectId: "chatapp-8f1df",
+  storageBucket: "chatapp-8f1df.appspot.com",
+  messagingSenderId: "816560297114",
+  appId: "1:816560297114:web:abacae15dcf0eed2e7bb23",
+  measurementId: "G-WTGXF4N801"
+};
+
+*/
--- components/Chat.js
+++ components/Chat.js
@@ -1,77 +1,79 @@
+import { onSnapshot, addDoc, query, collection, orderBy } from 'firebase/firestore';
 import { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, View, KeyboardAvoidingView } from 'react-native';
 import { Bubble, GiftedChat } from "react-native-gifted-chat";
 
-const Chat = ({ route, navigation }) => {
-  const { name } = route.params;
-  const { color } = route.params;
-  const [messages, setMessages] = useState([]);
-  const onSend = (newMessges) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessges))
-  }
-  const renderBubble = (props) => {
-    return <Bubble{...props}
-      wrapperStyle={{
-        right: {
-          backgroundColor: "#474056"
-        },
-        left: {
-          backgroundColor: "#FFF"
+const Chat = ({ db, route, navigation }) => {
+    const { userId } = route.params;
+    const { name } = route.params;
+    const { color } = route.params;
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        navigation.setOptions({ title: name });
+        navigation.setOptions({ backgroundColor: color });
+    }, []);
+
+    useEffect(() => {
+        navigation.setOptions({ title: name });
+        const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+
+        const unsubMessages = onSnapshot(q, (documentsSnapShot) => {
+            let messages = [];
+            documentsSnapShot.forEach(doc => {
+                messages.push({ id: doc.id, ...doc.data(), createdAt: new Date(doc.data().createdAt.toMillis()), });
+            })
+            setMessages(messages);
+        })
+
+        return () => {
+            if (unsubMessages) unsubMessages();
         }
-      }}
-    />
-  }
+    }, []);
 
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: "Hello developer",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
-        },
-      },
-      {
-        _id: 2,
-        text: 'This is a system message',
-        createdAt: new Date(),
-        system: true,
-      }
-    ]);
-  }, []);
 
-  useEffect(() => {
-    navigation.setOptions({ title: name });
-    navigation.setOptions({ backgroundColor: color });
+    const onSend = (newMessages) => {
+        addDoc(collection(db, "messages"), newMessages[0]);
+    };
 
-  }, []);
+    const renderBubble = (props) => {
+        return <Bubble{...props}
+            wrapperStyle={{
+                right: {
+                    backgroundColor: "#474056"
+                },
+                left: {
+                    backgroundColor: "#FFF"
+                }
+            }}
+        />
+    }
 
-  return (
-    <View style={styles.container}>
-      <GiftedChat
-        messages={messages}
-        renderBubble={renderBubble}
-        onSend={messages => onSend(messages)}
-        user={{
-          _id: 1
-        }}
-      />
-      {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
-    </View>
-  )
+
+    return (
+        <View style={[styles.container, { flex: 1, backgroundColor: color }]}>
+            <GiftedChat
+                messages={messages}
+                renderBubble={renderBubble}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: userId,
+                    name: name
+                }}
+            />
+            {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  text: {
-    color: 'white',
-    fontSize: 20
-  }
+    container: {
+        flex: 1,
+    },
+    text: {
+        color: 'white',
+        fontSize: 20
+    }
 });
 
 export default Chat;
